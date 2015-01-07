5589ae4d077b482b449f8ce36e80e4263b963dc5
Seane235 <seane235@gmail.com>
2015-01-06 23:00:10 -0500

    api response testing

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ app.controller('WeatherCtrl', function($http, $scope){
 	$scope.getWeather = function(){
 		$http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%2211211%22&format=json&diagnostics=true&callback=')
 		.success(function(data){
-			console.log(data.query.results.channel.item.condition)
+			// console.log(data.query.results.channel.item.condition)
 		})
 		.error(function(err){
-			console.log(err)
+			// console.log(err)
 		})
 	}
 })
