a89bd11634cec2c5e92c9a62277b527f741366dc
Seane235 <seane235@gmail.com>
2015-01-07 16:31:57 -0500

    modify success data testing

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 var app = angular.module('stratusApp', [])
 
 app.controller('WeatherCtrl', function($http, $scope){
-	$scope.getWeather = function(){
-		$http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%2211211%22&format=json&diagnostics=true&callback=')
+	$scope.getWeather = function(zip){
+		$http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%22' + zip + '%22&format=json&diagnostics=true&callback=')
 		.success(function(data){
 			// console.log(data.query.results.channel.item.condition)
+			console.log(data.query.results.channel.description)
+			// console.log(zip)
 		})
 		.error(function(err){
 			// console.log(err)
